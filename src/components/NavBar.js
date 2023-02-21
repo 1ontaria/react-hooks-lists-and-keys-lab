c7e291830696e04701f9c11a2339b65e5657e5ee
Ontaria Woods <ontaria.woods@gmail.com>
2023-02-21 03:19:39 -0500

    commit tree 53613ca36c18d27be0928ed7c425100eedc7ba05

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkElements = links.map((element) => {
+    return (
+      <a href={"#" + element} key={element}>
+        {element}
+      </a>
+    );
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linkElements}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techSpan = technologies.map((element) => {
+    return <span key={element.id}>{element}</span>;
+  });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{techSpan}</div>
     </div>
   );
 }
